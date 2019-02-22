674822ee9c77db049ed1fc946c77d91fda204e88
justine <you@example.com>
2019-02-22 15:54:37 +0100

    suppression de la ligne 9

--- functions.php
+++ functions.php
@@ -6,7 +6,3 @@ wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ));
 }
 ?>
-
-
-
-ytreftgyuhrft
